47da2ce4e99f0b6ef6f59fdfa76f2ec5bcd6f110
Simon Bédard <JavaKhanStudio@Gmail.com>
2025-05-08 04:47:25 +0200

    Adding Controller

---
+++ bucket/src/main/java/com/expoai/bucket/controller/PingController.java
@@ -0,0 +1,15 @@
+package com.expoai.bucket.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class PingController {
+
+    @GetMapping("/ping")
+    public String ping() {
+        return "pong";
+    }
+
+
+}
