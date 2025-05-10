9c24e3944259ebf36669c37e050c7c33f8a13a92
Simon Bédard <JavaKhanStudio@Gmail.com>
2025-05-10 02:25:28 +0200

    More testing in prod

--- bucket/src/main/java/com/expoai/bucket/atools/SetupController.java
+++ bucket/src/main/java/com/expoai/bucket/atools/SetupController.java
@@ -36,7 +36,7 @@ public class SetupController {
         admin.setUsername(request.username());
         admin.setPassword(passwordEncoder.encode(request.password()));
         Role role = new Role () ;
-        role.setId(0L);
+        role.setId(1L);
         admin.setRoles(List.of(role));
 
         userRepo.save(admin);
